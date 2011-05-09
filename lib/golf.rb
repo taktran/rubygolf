9420e75b1fbb0af8fd2859f5f8ada23fe9353aae
Tak <ttt@tbook.local>
2011-05-09 19:32:06 +0100

    hole 2 done

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,2 +1,17 @@
 class Golf
+  def Golf.hole2(line)
+    list = line.split(' ')
+    sorted_second_char_list = list.map{|elem| elem[1..elem.length]}.sort
+
+    sorted_hash = Hash.new
+    sorted_second_char_mapping = list.map{|elem| sorted_hash[elem[1..elem.length]] = elem}.sort
+    
+    return_array = []
+    for elem in sorted_second_char_list
+      return_array << sorted_hash[elem]
+    end
+                
+    return return_array.join(" ")
+  end
+  
 end
